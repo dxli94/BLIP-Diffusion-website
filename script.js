d7ae46eba9f52394da0608fc5b09e5daa493a81f
HUANGJUNZHE <huangjunzhe@HUANGJUNZHEs-MacBook-Air.local>
2023-05-21 22:25:52 +0800

    set the json

--- script.js
+++ script.js
@@ -1,151 +1,105 @@
-// import jsonData from "./data.json"
+"use strict"
 
-// const originalImage = new Set(jsonData.originalImage)
-// const easy_images = jsonData.easy_images
-// const hard_images = jsonData.hard_images
-// const test_easy = jsonData.test_easy
-// const test_hard = jsonData.test_hard
+fetch("data.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    const originalImage = new Set(jsonData.originalImage)
+    const easy_images = jsonData.easy_images
+    const hard_images = jsonData.hard_images
+    const test_easy = jsonData.test_easy
+    const test_hard = jsonData.test_hard
 
-function renderHard(imageContainer, data) {
-  imageContainer.innerHTML = ""
-  for (let i = 0; i < data.length; i++) {
-    const img = document.createElement("img")
-    img.src = data[i]
-    img.style.width = "120px"
-    imageContainer.appendChild(img)
-    img.classList.add("images")
-  }
-}
-function renderEasy(imageContainer, data) {
-  imageContainer.innerHTML = ""
-  for (let i = 0; i < data.length; i++) {
-    const img = document.createElement("img")
-    img.src = data[i]
-    img.style.width = "320px"
-    imageContainer.appendChild(img)
-    img.classList.add("images")
-  }
-}
+    function renderHard(imageContainer, data) {
+      imageContainer.innerHTML = ""
+      for (let i = 0; i < data.length; i++) {
+        const img = document.createElement("img")
+        img.src = data[i]
+        img.style.width = "120px"
+        imageContainer.appendChild(img)
+        img.classList.add("images")
+      }
+    }
+    function renderEasy(imageContainer, data) {
+      imageContainer.innerHTML = ""
+      for (let i = 0; i < data.length; i++) {
+        const img = document.createElement("img")
+        img.src = data[i]
+        img.style.width = "320px"
+        imageContainer.appendChild(img)
+        img.classList.add("images")
+      }
+    }
 
-const originalImage = new Set(["a-backpack-in-the-style-of-teddybear_0(1).jpg"])
+    const imageContainer = document.querySelectorAll(".imageContainer")
 
-const easy_images = [
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-]
+    const hardButton = document.querySelector(".hardButton")
 
-const hard_images = [
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0(1).jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0(1).jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0(1).jpg",
-]
-const test_easy = [
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0(1).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-]
-const test_hard = [
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0.jpg",
-  "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg",
-  "./data/a-backpack-in-the-style-of-teddybear_0(1).jpg",
-]
+    const easyButton = document.querySelector(".easyButton")
 
-const imageContainer = document.querySelectorAll(".imageContainer")
+    // Function to add the additional picture
+    function addAdditionalPicture() {
+      renderHard(imageContainer[0], hard_images)
+      renderHard(imageContainer[1], test_hard)
 
-const hardButton = document.querySelector(".hardButton")
+      flipImg()
 
-const easyButton = document.querySelector(".easyButton")
+      // Disable the button after adding the picture
+      hardButton.disabled = true
+      easyButton.disabled = false
+    }
+    function getInitialPicture() {
+      renderEasy(imageContainer[0], easy_images)
+      renderEasy(imageContainer[1], test_easy)
 
-// Function to add the additional picture
-function addAdditionalPicture() {
-  // // Create a new image element
-  // const additionalImage = document.createElement("img")
-  // additionalImage.src =
-  //   "./Multi-Concept Customization of Text-to-Image Diffusion_files/dog-wearing-sunglasses_2(2).jpg"
-  // additionalImage.alt = "Image 4"
+      flipImg()
 
-  // // Append the image to the container
-  // imageContainer.appendChild(additionalImage)
-  // imageContainer.forEach((imageContainer) => {
-  //   imageContainer.innerHTML = ""
-  //   for (let i = 0; i < hard_images.length; i++) {
-  //     const img = document.createElement("img")
-  //     img.src = hard_images[i]
-  //     img.style.width = "120px"
-  //     imageContainer.appendChild(img)
-  //     img.classList.add("images")
-  //   }
-  // })
+      hardButton.disabled = false
+      easyButton.disabled = true
+    }
 
-  renderHard(imageContainer[0], hard_images)
-  renderHard(imageContainer[1], test_hard)
+    // Add event listener to the button
+    hardButton.addEventListener("click", addAdditionalPicture)
+    easyButton.addEventListener("click", getInitialPicture)
 
-  flipImg()
+    function flipImg() {
+      const images = document.getElementsByClassName("images")
 
-  // Disable the button after adding the picture
-  hardButton.disabled = true
-  easyButton.disabled = false
-}
-function getInitialPicture() {
-  renderEasy(imageContainer[0], easy_images)
-  renderEasy(imageContainer[1], test_easy)
-
-  flipImg()
-
-  hardButton.disabled = false
-  easyButton.disabled = true
-}
-
-// Add event listener to the button
-hardButton.addEventListener("click", addAdditionalPicture)
-easyButton.addEventListener("click", getInitialPicture)
-
-function flipImg() {
-  const images = document.getElementsByClassName("images")
-
-  for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", flipImage)
-    images[i].addEventListener("mouseleave", revertImage)
-  }
-
-  function flipImage() {
-    if (this.classList.contains("flipped")) {
-      // Flip back to the original image
-      this.src = this.dataset.originalSrc
-    } else {
-      // Flip to a different image
-      this.dataset.originalSrc = this.src // Store the original image source
-      console.log(this.dataset.originalSrc)
-      if (originalImage.has(this.dataset.originalSrc.split("/").pop())) {
-        this.src = "./data/true.png"
-      } else {
-        this.src = "./data/false.png"
+      for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener("click", flipImage)
+        images[i].addEventListener("mouseleave", revertImage)
       }
 
-      // this.src = "./data/a-backpack-in-the-style-of-tortoise-plushy_2(2).jpg" // Replace with the new image source
-    }
-    this.classList.toggle("flipped")
-  }
-  function revertImage() {
-    if (this.classList.contains("flipped")) {
-      this.src = this.dataset.originalSrc
-      this.classList.remove("flipped")
-    }
-  }
-}
+      function flipImage() {
+        if (this.classList.contains("flipped")) {
+          // Flip back to the original image
+          this.src = this.dataset.originalSrc
+        } else {
+          // Flip to a different image
+          this.dataset.originalSrc = this.src // Store the original image source
+          console.log(this.dataset.originalSrc)
+          if (originalImage.has(this.dataset.originalSrc.split("/").pop())) {
+            this.src = "./data/true.png"
+          } else {
+            this.src = "./data/false.png"
+          }
 
-flipImg()
+          // Replace with the new image source
+        }
+        this.classList.toggle("flipped")
+      }
+      function revertImage() {
+        if (this.classList.contains("flipped")) {
+          this.src = this.dataset.originalSrc
+          this.classList.remove("flipped")
+        }
+      }
+    }
 
-// for the first render
-addAdditionalPicture()
+    flipImg()
+
+    // for the first render
+    addAdditionalPicture()
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error)
+  })
